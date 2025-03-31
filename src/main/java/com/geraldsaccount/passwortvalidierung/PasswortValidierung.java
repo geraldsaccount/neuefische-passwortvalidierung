7f35984a9fa2a4deb5f4ff158bb4abb0f7771047
gerald <geraldmontufar@gmail.com>
2025-03-31 09:31:22 +0200

    added method signatures for needed validation methods

--- src/main/java/com/geraldsaccount/passwortvalidierung/PasswortValidierung.java
+++ src/main/java/com/geraldsaccount/passwortvalidierung/PasswortValidierung.java
@@ -13,4 +13,24 @@ public class PasswortValidierung {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static boolean isValidPassword(String password) {
+        return false;
+    }
+
+    public static boolean isLengthValid(String password) {
+        return false;
+    }
+
+    public static boolean containsDigit(String password) {
+        return false;
+    }
+
+    public static boolean containsUppercaseAndLowercase(String password) {
+        return false;
+    }
+
+    public static boolean isCommonPassword(String password) {
+        return false;
+    }
 }
